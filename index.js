fdce75ddd0b37f4a7c080ef9024d88c99e89c8f5
April <aprilww@umich.edu>
2022-02-18 10:18:07 -0500

    ## Step 4. Add a "Start" Button
    We then implement a start button to control the stopwatch. We also want to disable the start button when it is clicked. This can be done by setting the `disabled` attribute in JavaScript.

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const timeDisplay = document.getElementById("time-display");
-const loadTime = (new Date()).getTime();
+const startButton = document.getElementById("start-button");
 
+let lastTime;
 let clock = 0;
+let countInterval;
 
 function render() {
     timeDisplay.textContent = Math.round(clock / 1000);
 }
 function updateClock() {
-    clock = new Date().getTime() - loadTime;
+    clock = new Date().getTime() - lastTime;
     render();
 }
-const countInterval = setInterval(updateClock, 1000);
+startButton.addEventListener("click", () => {
+    lastTime = new Date().getTime();
+    countInterval = setInterval(updateClock, 1000);
+    startButton.setAttribute("disabled", "true");
+});
