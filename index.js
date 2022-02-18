5572e271748b4becd4e2cb1aa77b8d8fb906f9dd
April <aprilww@umich.edu>
2022-02-18 10:15:12 -0500

    ## Step 3. Display the Number of Seconds since Loading the Page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const timeDisplay = document.getElementById("time-display");
+const loadTime = (new Date()).getTime();
 
 let clock = 0;
 
@@ -6,7 +7,7 @@ function render() {
     timeDisplay.textContent = Math.round(clock / 1000);
 }
 function updateClock() {
-    clock = new Date().getTime();
+    clock = new Date().getTime() - loadTime;
     render();
 }
 const countInterval = setInterval(updateClock, 1000);
